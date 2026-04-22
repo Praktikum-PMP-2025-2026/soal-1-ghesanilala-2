08fef186c64dc879602ed5fbd8610ff30862d770
ghesanilala <ghaysaniathaya01@gmail.com>
2026-04-22 10:42:08 +0700

    soal1.c

--- soal1.c
+++ soal1.c
@@ -14,13 +14,13 @@ void average(int *ptr, int n, int *sum, double *avg){
   *avg = temp;
 }
 
-void maks(int *ptr, int n, int sum, int *max, int *idx){
+void maks(int *ptr, int n, int *max, int *idx){
   *max = ptr[0];
   *idx = 0;
   for(int i=0; i<n; i++){
-    if(ptr[i] < ptr[i + 1]){
-      *max = ptr [i+1];
-      *idx = i +1;
+    if(ptr[i] > *max ){
+      *max = ptr[i];
+      *idx = i ;
     }
   }
 }
@@ -37,7 +37,7 @@ int main(){
   double avg;
   jumlah(ptr, n, &sum);
   average(ptr, n, &sum, &avg);
-  maks(ptr,n, sum, &max, &idx);
+  maks(ptr,n, &max, &idx);
   printf("SUM %d\n", sum);
   printf("AVG %.2f\n", avg);
   printf("MAX %d\n", max);
