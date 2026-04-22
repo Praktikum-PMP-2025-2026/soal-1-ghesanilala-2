2e4d7d3a1d8150fee29457f27ff4b9858453efa8
ghesanilala <ghaysaniathaya01@gmail.com>
2026-04-22 10:52:57 +0700

    soal1.c

--- soal1.c
+++ soal1.c
@@ -1,3 +1,17 @@
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : 2
+ *   Hari dan Tanggal    : rabu, 22 april 2026
+ *   Nama (NIM)          : najwa ghaysani athaya a (13224023)
+ *   Nama File           : soal1.c
+ *   Deskripsi           :  menerima deretan data eksperimen dan harus segera merangkum 
+statistik pentingnya untuk operator. Namun, modul analisis yang digunakan di laboratorium itu 
+dirancang agar seluruh hasil dikembalikan melalui alamat memori yang diberikan ke fungsi, bukan 
+melalui nilai balik biasa. Anda diminta membantu menyiapkan fungsi analisis tersebut agar mesin 
+dapat melaporkan jumlah, rata-rata, nilai maksimum, dan posisi pertama kemunculan nilai 
+maksimum dengan benar.
+ * 
+ */
+
 #include <stdio.h>
 
 void jumlah(int *ptr, int n, int *sum){
